043c85893afaae516f818ae284b6652a5ccf7672
MUKILTEO\1611356 <1611356@mukilteo.wednet.edu>
2025-03-26 13:40:19 -0700

    worked on NPC movement

---
+++ alien game/Assets/Sulaiman/Scrips/A star managerrr.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Astarmanagerrr : MonoBehaviour
+{
+    public static Astarmanagerrr instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public List<Node> 
+}
