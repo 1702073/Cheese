d830a0e88fb1eda548849628ede71384748e9996
MUKILTEO\1611356 <1611356@mukilteo.wednet.edu>
2025-03-28 12:26:42 -0700

    more Ai stuff

--- alien game/Assets/Sulaiman/Scrips/A star managerrr.cs
+++ alien game/Assets/Sulaiman/Scrips/A star managerrr.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Astarmanagerrr : MonoBehaviour
 {
@@ -9,5 +10,43 @@ public class Astarmanagerrr : MonoBehaviour
         instance = this;
     }
 
-    public List<Node> 
+    [System.Obsolete]
+    public List<Node>  GeneratePath(Node start, Node end)
+    {
+        List<Node> openSet = new List<Node>();
+
+        foreach (Node n in FindObjectsOfType<Node>())
+        {
+            n.gScore = float.MaxValue;
+        }
+
+        start.gScore = 0;
+        start.hScore = Vector2.Distance(start.transform.position, end.transform.position);
+        openSet.Add(start);
+
+        while(openSet.Count > 0)
+        {
+            int lowestF = default;
+
+            for(int i = 1; i < openSet.Count; i++)
+            {
+                if(openSet[i].FScore() < openSet[lowestF].FScore())
+                {
+                    lowestF = i;
+                }
+            }
+
+            Node current = openSet[lowestF];
+            openSet.Remove(current);
+
+            if (current == end)
+            {
+                List<Node> path = new List<Node>();
+
+                path.Insert(0, end);
+            }
+        }
+
+        return null;
+    }
 }
