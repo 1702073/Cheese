bd4ffe94fb4ab5a7c8852a484a0fe96d9e470cdf
MUKILTEO\1704174 <1704174@mukilteo.wednet.edu>
2025-04-01 13:34:42 -0700

    Added background music

--- alien game/Assets/John/Scripts/ClockTimer.cs
+++ alien game/Assets/John/Scripts/ClockTimer.cs
@@ -3,23 +3,47 @@ using UnityEngine.UI;
 
 public class ClockTimer : MonoBehaviour
 {
-    public Text timer;
-    float currentvalue;
+   [SerializeField]
+    public float totalTime = 10f; // Total countdown time in seconds
+    public Text countdownText; // UI Text element to display the timer
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float currentTime;
+    private bool countdownStarted = false;
+
     void Start()
     {
-        currentvalue = 60f;
+        currentTime = totalTime;
+   
     }
 
-    // Update is called once per frame
     void Update()
     {
-        currentvalue-= 1 * Time.deltaTime;
-        timer.text = currentvalue.ToString("0");
-        if (currentvalue <= 0)
+        if (countdownStarted)
         {
-            timer.text = "End";
+            currentTime -= 1 * Time.deltaTime;
+            countdownText.text = Mathf.Ceil(currentTime).ToString(); // Display as a whole number
+
+            if (currentTime <= 0)
+            {
+                // Countdown finished, do something here
+                Debug.Log("Countdown finished!");
+                // You can add code here to start the game, etc.
+                countdownStarted = false; // Stop the countdown
+            }
+        }
+      
+    }
+
+    // Called when the player enters the trigger collider
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Check if the colliding object is the player
+        {
+            countdownStarted = true;
         }
     }
+    
+    
+
+   
 }
