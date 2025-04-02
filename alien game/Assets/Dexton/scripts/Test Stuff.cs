dd06ab05543005ecf85b6baf410a56704cfaefff
MUKILTEO\1702073 <1702073@mukilteo.wednet.edu>
2025-04-02 13:34:50 -0700

    credit scene ect

--- alien game/Assets/Dexton/scripts/Test Stuff.cs
+++ alien game/Assets/Dexton/scripts/Test Stuff.cs
@@ -4,15 +4,14 @@ using UnityEngine.InputSystem;
 public class TestStuff : MonoBehaviour
 {
     Vector2 Vector2 = new Vector2(1, 1);
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Vector2 = Mouse.current.position.ReadValue();
+        Vector2 =  Mouse.current.position.ReadValue();
     }
 
     void Update()
     {
-        transform.position = Vector2;
-        //Mouse.current.WarpCursorPosition(Vector2.right);
+        Mouse.current.WarpCursorPosition(Vector2.right);
+        Vector2 = transform.position;
     }
 }
