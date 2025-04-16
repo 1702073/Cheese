8a5740f84f5901e917aff78cde5b9ada24119a4a
MUKILTEO\1611356 <1611356@mukilteo.wednet.edu>
2025-04-16 13:03:36 -0700

    more work Johns stuff

--- alien game/Assets/John/Scripts/DragAndDrop.cs
+++ alien game/Assets/John/Scripts/DragAndDrop.cs
@@ -6,6 +6,7 @@ public class DragAndDrop : MonoBehaviour,IDragHandler,IBeginDragHandler,IEndDrag
     private RectTransform rectTransform;
     private Image image;
 
+    private Camera cam;
     public void OnBeginDrag(PointerEventData eventData)
     {
         throw new System.NotImplementedException();
@@ -29,7 +30,23 @@ public class DragAndDrop : MonoBehaviour,IDragHandler,IBeginDragHandler,IEndDrag
     {
         rectTransform = GetComponent<RectTransform>();
         image = GetComponent<Image>();
-    }
 
+        cam = Camera.main;
+    }
+    void onGUI()
+    {
+        Vector3 point = new Vector3();
+        Event currentEvent = Event.current;
+        Vector2 mousePos = new Vector2();
+        mousePos.x = currentEvent.mousePosition.x;
+        mousePos.y = currentEvent.mousePosition.y;
+        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x,mousePos.y, cam.nearClipPlane));
+
+        GUILayout.BeginArea(new Rect(mousePos.x, mousePos.y, 100, 100));
+        GUILayout.Label("Screen pixels:" + cam.pixelWidth + ":" + cam.pixelHeight);
+        GUILayout.Label("Mousew.position:" + mousePos);
+        GUILayout.Label("Mousew.position:" + point.ToString("F3"));
+        GUILayout.EndArea();
+    }
 
 }
