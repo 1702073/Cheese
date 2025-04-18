56cc070ef29adb94ecdb0ce957f10b0e670c466d
MUKILTEO\1704174 <1704174@mukilteo.wednet.edu>
2025-04-18 12:21:41 -0700

    didnt get much done

--- alien game/Assets/John/Scripts/Actions.cs
+++ alien game/Assets/John/Scripts/Actions.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
-public class PlayerItemCollector : MonoBehaviour
+public class Actions : MonoBehaviour
 {
+    private enum eStates { IDLE, GOTO, OPEN, GET };
+    private eStates currentState = eStates.IDLE;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
