5ad8f4f6ff5c8cba122380a13dedfe074d35db74
MUKILTEO\1702073 <1702073@mukilteo.wednet.edu>
2025-04-22 13:38:44 -0700

    🦐

--- alien game/Assets/NpcAI.cs
+++ alien game/Assets/NpcAI.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using Pathfinding;
+using UnityEngine.AI;
 
 public class NpcAI : MonoBehaviour
 {
-    public Transform target;
-    public float speed = 200f;
-    public float nextWaypointDistance = 3f;
-    
+    public Vector2 target;
+    public float speed = 200f, nextWaypointDistance = 3f;
+
+    [SerializeField]Vector2 minPosition, maxPosition;
+
     public Transform npcGFX;
 
     Path path;
@@ -24,17 +26,21 @@ public class NpcAI : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
         InvokeRepeating("UpdatePath", 0f, .5f);
-        
     }
 
     void UpdatePath()
     {
         if (seeker.IsDone())
-            seeker.StartPath(rb.position, target.position, OnPathComplete);
-
-        
+        {
+            seeker.StartPath(rb.position, target, OnPathComplete);
+            SetRandomTarget();
+        }
+                    
     }
-
+    void SetRandomTarget()
+    {
+        Vector2 target = new Vector2(Random.Range(minPosition.x, maxPosition.x), Random.Range(minPosition.y, maxPosition.y));
+    }
 
     void OnPathComplete(Path p)
     {
@@ -57,30 +63,23 @@ public class NpcAI : MonoBehaviour
             reachedEndOfPath = true;
             return;
         }else
-        {
+         { 
             reachedEndOfPath = false;
-        }
-
-        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
-        Vector2 force = direction * speed * Time.deltaTime;
+         }
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position);
+        Vector2 force = (direction * speed * Time.deltaTime).normalized;
 
         rb.AddForce(force);
-
 
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
 
         if (distance < nextWaypointDistance)
-        {
             currentWaypoint++;
-        }
 
         if (force.x >= 0.01f)
-        {
             npcGFX.localScale = new Vector3(1, 1, 1);
-        }
         else if (force.x <= -0.01f)
-        {
             npcGFX.localScale = new Vector3(-1, 1, 1);
-        }
     }
+
 }
