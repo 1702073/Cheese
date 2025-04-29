822e425623d26e9c3116129c73fc124a3dc94e90
MUKILTEO\1702073 <1702073@mukilteo.wednet.edu>
2025-04-29 13:37:36 -0700

    🐭🦁 mwow

--- alien game/Assets/Dexton/scripts/NPC_state_Change.cs
+++ alien game/Assets/Dexton/scripts/NPC_state_Change.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class NPC_state_Change : MonoBehaviour
 {
+    int[] honkshoua = { 1, 4, 3, 2, 6, 12};
+
+
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
